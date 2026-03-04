f5346f5ba7b299640707daa6afb7e2a1305055e9
Relixsx <65338977+Relixsx@users.noreply.github.com>
2026-03-04 14:00:17 +0100

    Update engine.py

--- ToolKit/engine.py
+++ ToolKit/engine.py
@@ -127,8 +127,8 @@ def train(model: torch.nn.Module,
           optimizer: torch.optim.Optimizer,
           loss_fn: torch.nn.Module,
           epochs: int,
-          schedular_lr: Optional[torch.optim.lr_scheduler._LRScheduler] = None,
-          device: torch.device) -> Dict[str, List]:
+          device: torch.device,
+          schedular_lr: Optional[torch.optim.lr_scheduler._LRScheduler] = None) -> Dict[str, List]:
     """Trains and tests a PyTorch model.
 
     Passes a target PyTorch models through train_step() and test_step()
